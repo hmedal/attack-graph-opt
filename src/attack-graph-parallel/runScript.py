7f906c2fb5b04384185749b7a2e3b452df8a33c7
hmedal <hugh.medal@gmail.com>
2016-09-30 13:10:46 -0500

    Rearranging of files in the directory.

--- src/attack-graph-parallel/runScript.py
+++ src/attack-graph-parallel/runScript.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     max_iter = 10
     for iter in range(1,max_iter + 1):
         os.system('mpirun -n 1 -hosts ' + args.master + ' gurobi_cl WorkerPool=' + args.worker_pool + ' DistributedMIPJobs=' + args.count + ' ResultFile=' + 'master_' + iter + '.sol' + ' /usr/local/gurobi652/linux64/examples/data/misc07.mps') # solve master problem
-        os.system('mpirun -np ' + str(numProcs) + ' python subProbs.py master_' + str(iter) + '.sol') # solve sub problem
+        #os.system('mpirun -np ' + str(numProcs) + ' python subProbs.py master_' + str(iter) + '.sol') # solve sub problem
         # read upper bound from a file
         # read lower bound from a file
         # check termination; print final solution and objective value to file
